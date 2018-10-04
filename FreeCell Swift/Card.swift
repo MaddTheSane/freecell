16231aafdf2708ceb78c9a5786bcbc16004d65c7
C.W. Betts <computers57@hotmail.com>
2018-10-04 15:44:38 -0600

    More pokes.

--- FreeCell Swift/Card.swift
+++ FreeCell Swift/Card.swift
@@ -7,26 +7,112 @@
 
 import Foundation
 
-/// Suit order is important: must match the card graphics file.
-public enum Suit : Int32 {
-    case clubs = 0
-    case diamonds
-    case hearts
-    case spades
+struct Card: Hashable, CustomStringConvertible {
+    /// Suit order is important: must match the card graphics file.
+    enum Suit: Int32, CustomStringConvertible {
+        case clubs = 0
+        case diamonds
+        case hearts
+        case spades
+        
+        var description: String {
+            switch self {
+                //@"Clubs", @"Diamonds", @"Hearts", @"Spades"
+                
+            case .clubs:
+                return "Clubs"
+            case .diamonds:
+                return "Diamonds"
+            case .hearts:
+                return "Hearts"
+                
+            case .spades:
+                return "Spades"
+            }
+        }
+        
+        var isRed: Bool {
+            return self == .hearts || self == .diamonds
+        }
+        
+        var isBlack: Bool {
+            return self == .clubs || self == .spades
+        }
+    }
+    
+    enum Rank: Int32 {
+        case ace = 1
+        case two
+        case three
+        case four
+        case five
+        case six
+        case seven
+        case eight
+        case nine
+        case ten
+        case jack
+        case queen
+        case king
+        
+        var description: String {
+            switch self {
+            case .ace:
+                return "Ace"
+                
+            case .two:
+                return "Two"
+                
+            case .three:
+                return "Three"
+                
+            case .four:
+                return "Four"
+                
+            case .five:
+                return "Five"
+                
+            case .six:
+                return "Six"
+                
+            case .seven:
+                return "Seven"
+                
+            case .eight:
+                return "Eight"
+                
+            case .nine:
+                return "Nine"
+                
+            case .ten:
+                return "Ten"
+                
+            case .jack:
+                return "Jack"
+                
+            case .queen:
+                return "Queen"
+                
+            case .king:
+                return "King"
+            }
+        }
+    }
+
+    var suit: Suit
+    var rank: Rank
+    
+    var description: String {
+        return "\(rank) of \(suit)"
+    }
 }
 
-public enum Rank : Int32 {
-    case ace = 1
-    case two
-    case three
-    case four
-    case five
-    case six
-    case seven
-    case eight
-    case nine
-    case ten
-    case jack
-    case queen
-    case king
+extension Card {
+    var isRed: Bool {
+        return suit.isRed
+    }
+    
+    var isBlack: Bool {
+        return suit.isBlack
+    }
 }
