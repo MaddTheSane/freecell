8f7c6be5ae6cc11f8ef817032f5e2598b1382728
C.W. Betts <computers57@hotmail.com>
2018-10-26 23:38:25 -0600

    More implementation of PreferencesController.

--- FreeCell Swift/PreferencesController.swift
+++ FreeCell Swift/PreferencesController.swift
@@ -14,16 +14,35 @@ class PreferencesController: NSObject {
 	@IBOutlet weak var backgroundColour: NSColorWell!
 	@IBOutlet weak var window: NSWindow!
 
+	override func awakeFromNib() {
+		let defaults = UserDefaults.standard
+		defaults.register(defaults: ["gameSuperMove": true, "gameAutoStack": true])
+		
+		autoStack.state = defaults.bool(forKey: "gameAutoStack") ? .on : .off
+		superMove.state = defaults.bool(forKey: "gameSuperMove") ? .on : .off
+		
+		if let data = defaults.data(forKey: "backgroundColour"), let colour = NSKeyedUnarchiver.unarchiveObject(with: data) as? NSColor {
+			backgroundColour.color = colour
+		}
+	}
+	
 	@IBAction func openWindow(_ sender: Any?) {
-		
+		window.makeKeyAndOrderFront(self)
 	}
+	
 	@IBAction func autoStackClicked(_ sender: Any?) {
-		
+		let state = autoStack.state == .on
+		UserDefaults.standard.set(state, forKey: "gameAutoStack")
 	}
+	
 	@IBAction func superMoveClicked(_ sender: Any?) {
-		
+		let state = superMove.state == .on
+		UserDefaults.standard.set(state, forKey: "gameSuperMove")
 	}
+	
 	@IBAction func backgroundColourChosen(_ sender: Any?) {
-		
+		let colour = backgroundColour.color
+		UserDefaults.standard.set(NSKeyedArchiver.archivedData(withRootObject: colour), forKey: "backgroundColour")
+		gameView.backgroundColor = colour
 	}
 }
